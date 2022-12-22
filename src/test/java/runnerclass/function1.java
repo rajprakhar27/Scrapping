d9cf251f7ccee2b1a924ae7f95253cb51e3ec69c
dell <dell@DESKTOP-EBUR89J>
2022-12-23 03:03:41 +0530

    commit

--- src/test/java/runnerclass/function1.java
+++ src/test/java/runnerclass/function1.java
@@ -7,6 +7,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
